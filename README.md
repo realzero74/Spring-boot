1f4ad6741e67538188b2a4038f12e178f46b3136
jinyoung <realzero74@gmail.com>
2023-01-29 01:06:29 +0900

    readme 컨셉 정의

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # spring-boot-restapi
+
+## get
+### 파라미터별 VO(Dto) Map request객체 
+### 리턴 타입별 String VO(DTO) Map ResponseEntity
+
+## post
+### 파라미터별
+### 리턴타입별
+### contents/type별 
+
+## put
+
+## delete
--- src/main/java/com/example/springbootrestapi/SpringBootRestapiApplication.java
+++ src/main/java/com/example/springbootrestapi/SpringBootRestapiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRestapiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootRestapiApplication.class, args);
+        System.out.println("start SpringBootRestapiApplication================================================");
     }
 
 }
